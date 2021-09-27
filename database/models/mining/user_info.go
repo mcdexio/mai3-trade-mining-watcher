8f1f6db9fafe4310a81ca15b925c87034bb72113
yangzhao <yangzhao@mcarlo.com>
2021-09-28 01:26:20 +0800

    optimize batch updating && retrieving mark prices

--- database/models/mining/user_info.go
+++ database/models/mining/user_info.go
@@ -7,8 +7,8 @@ import (
 
 // UserInfo defines struct to contain information of a user info
 type UserInfo struct {
-	ID            int64           `gorm:"column:id;primary_key;AUTO_INCREMENT;not null"`
-	Trader        string          `gorm:"column:trader;type:varchar(128);not null" json:"trader"`
+	Trader        string          `gorm:"column:trader;primary_key;type:varchar(128);not null" json:"trader"`
+	Epoch         int64           `gorm:"column:epoch;primary_key;type:bigint;not null" json:"epoch"`
 	InitFee       decimal.Decimal `gorm:"column:init_fee;type:decimal(38,18);not null" json:"init_fee"`
 	AccFee        decimal.Decimal `gorm:"column:acc_fee;type:decimal(38,18);not null" json:"acc_fee"`
 	AccPosValue   decimal.Decimal `gorm:"column:acc_pos_value;type:decimal(38,18);not null" json:"acc_pos_value"`
@@ -16,8 +16,8 @@ type UserInfo struct {
 	AccStakeScore decimal.Decimal `gorm:"column:acc_stake_score;type:decimal(38,18);not null" json:"acc_stake_score"`
 	CurStakeScore decimal.Decimal `gorm:"column:cur_stake_score;type:decimal(38,18);not null" json:"cur_stake_score"`
 	Score         decimal.Decimal `gorm:"column:score;type:decimal(38,18);not null" json:"score"`
-	Epoch         int64           `gorm:"column:epoch;type:bigint;not null" json:"epoch"`
-	Timestamp     int64           `gorm:"column:timestamp;type:bigint;not null" json:"timestamp"`
+
+	Timestamp int64 `gorm:"column:timestamp;type:bigint;not null" json:"timestamp"`
 
 	models.Base
 }
--- syncer/syncer.go
+++ syncer/syncer.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mcdexio/mai3-trade-mining-watcher/database/models/mining"
 	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"github.com/mcdexio/mai3-trade-mining-watcher/common/logging"
 	database "github.com/mcdexio/mai3-trade-mining-watcher/database/db"
@@ -72,7 +73,7 @@ type Block struct {
 }
 
 func NewSyncer(
-	ctx context.Context, logger logging.Logger, mai3GraphUrl string, blockGraphUrl string
+	ctx context.Context, logger logging.Logger, mai3GraphUrl string, blockGraphUrl string,
 	) *Syncer {
 	return &Syncer{
 		ctx:           ctx,
@@ -165,47 +166,58 @@ func (s *Syncer) Run() error {
 	}
 }
 
-func (s *Syncer) GetUsersBasedOnBlockNumber(blockNumber int64) ([]User, error) {
-	var users []User
-	s.logger.Info("get users based on block number %d", blockNumber)
+// getUserWithBlockNumberID try three times to get users depend on ID with order and filter
+func (s *Syncer) getUserWithBlockNumberID(blockNumber int64, id string) ([]User, error) {
+	// s.logger.Debug("Get users based on block number %d and order and filter by ID %s", blockNumber, id)
 	query := `{
-		users(first: 500, skip: %d, block: { number: %d }, where: {totalFee_gt: 0}) {
+		users(first: 1000, block: {number: %d}, orderBy: id, orderDirection: asc,
+			where: { id_gt: "%s" totalFee_gt: 0}
+		) {
 			id
 			stakedMCB
+			unlockMCBTime
 			totalFee
-			marginAccounts(where:{position_gt: 0}){
-	  			id
-	  			position
+			marginAccounts(where: { position_gt: 0}) {
+				id
+				position
 			}
 		}
 	}`
-	skip := 0
-	for {
-		var resp struct {
-			Data struct {
-				Users []User
-			}
-		}
-		if err := s.queryGraph(&resp, query, skip, blockNumber); err != nil {
-			return nil, fmt.Errorf("fail to get users on block: skip=%v, block=%v %w", skip, blockNumber, err)
-		}
-		users = append(users, resp.Data.Users...)
-		if len(resp.Data.Users) == 500 {
-			// means there are more data to get
-			skip += 500
-			if skip == 5000 {
-				return nil, fmt.Errorf("user more than 5000, but we don't have filter")
-			}
-		} else {
-			break
+	var response struct {
+		Data struct {
+			Users []User
 		}
 	}
-	return users, nil
+	// try three times for each pagination.
+	if err := s.queryGraph(s.mai3GraphUrl, &response, query, blockNumber, id); err != nil {
+		return []User{}, errors.New("failed to get users in three times")
+	}
+	return response.Data.Users, nil
 }
 
-func (s *Syncer) syncStatePreventRollback() {
-	s.logger.Info("Sync state until one hour ago for preventing rollback")
+func (s *Syncer) GetUsersBasedOnBlockNumber(blockNumber int64) ([]User, error) {
+	s.logger.Debug("Get users based on block number %d", blockNumber)
+	var retUser []User
+
+	idFilter := "0x0"
+	for {
+		users, err := s.getUserWithBlockNumberID(blockNumber, idFilter)
+		if err != nil {
+			return retUser, err
+		}
+		// success get user based on block number and idFilter
+		retUser = append(retUser, users...)
+		length := len(users)
+		if length == 1000 {
+			// means there are more users, update idFilter
+			idFilter = users[length-1].ID
+		} else {
+			// means got all users
+			return retUser, nil
+		}
+	}
 }
+
 
 func (s *Syncer) GetPoolAddrIndexUserID(marginAccountID string) (poolAddr, userId string, perpetualIndex int, err error) {
 	rest := strings.Split(marginAccountID, "-")
@@ -278,4 +290,300 @@ func (s *Syncer) initUserStates() error {
 	}
 	// end tx
 	return nil
+}
+
+// queryGraph return err if failed to get response from graph in three times
+func (s *Syncer) queryGraph(graphUrl string, resp interface{}, query string, args ...interface{}) error {
+	var params struct {
+		Query string `json:"query"`
+	}
+	params.Query = fmt.Sprintf(query, args...)
+	for i := 0; i < 3; i++ {
+		err, code, res := s.httpClient.Post(graphUrl, nil, params, nil)
+		if err != nil {
+			s.logger.Error("fail to post http request %w", err)
+			continue
+		} else if code/100 != 2 {
+			s.logger.Error("unexpected http response: %v", code)
+			continue
+		}
+		err = json.Unmarshal(res, &resp)
+		if err != nil {
+			s.logger.Error("failed to unmarshal %w", err)
+			continue
+		}
+		// success
+		return nil
+	}
+	return errors.New("failed to get queryGraph in three times")
+}
+
+func norm(ts int64) int64 {
+	return ts - ts%60
+}
+
+func normN(ts int64, inv int64) int64 {
+	return ts - ts%inv
+}
+
+func (s *Syncer) syncState() (int64, error) {
+	s.logger.Info("enter sync state")
+	defer s.logger.Info("leave sync state")
+
+	// begin tx
+	p, err := s.getSyncProgress()
+	if err != nil {
+		return 0, fmt.Errorf("fail to get sync progress %w", err)
+	}
+	np := norm(p + 60)
+	bn, err := s.TimestampToBlockNumber(np)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get block number from timestamp: timestamp=%v %w", np, err)
+	}
+	users, err := s.GetUsersBasedOnBlockNumber(bn)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get users on block number: blocknumber=%v %w", bn, err)
+	}
+	s.logger.Info("found %v users @%v", len(users), bn)
+	// 1. preprocess
+	{
+		// acc_pos_value <= cur_pos_value
+		err = s.db.Model(mining.UserInfo{}).
+			Where("epoch=? and cur_pos_value <> 0", s.thisEpoch).
+			UpdateColumn("acc_pos_value", gorm.Expr("acc_pos_value + cur_pos_value")).Error
+		if err != nil {
+			return 0, fmt.Errorf("failed to accumulate cur_post_value to acc_pos_value  %w", err)
+		}
+		// acc_stake_score <= cur_stake_score
+		err = s.db.Model(mining.UserInfo{}).
+			Where("epoch=? and cur_stake_score <> 0", s.thisEpoch).
+			UpdateColumn("acc_stake_score", gorm.Expr("acc_stake_score + cur_stake_score")).Error
+		if err != nil {
+			return 0, fmt.Errorf("failed to accumulate cur_stake_score to acc_stake_score %w", err)
+		}
+		// cur_stake_score <= 0
+		err = s.db.Model(mining.UserInfo{}).Where("epoch=?", s.thisEpoch).
+			Updates(mining.UserInfo{CurPosValue: decimal.Zero, CurStakeScore: decimal.Zero, Timestamp: np}).Error
+		if err != nil {
+			return 0, fmt.Errorf("failed to set cur_stake_score and cur_pos_value to 0 %w", err)
+		}
+	}
+	// 2. update graph data
+	prices, err := s.GetMarkPrices(bn)
+	if err != nil {
+		return 0, fmt.Errorf("fail to get mark prices %w", err)
+	}
+	uis := make([]*mining.UserInfo, len(users))
+	for i, u := range users {
+		pv, err := s.getPositionValue(u.MarginAccounts, bn, prices)
+		if err != nil {
+			return 0, fmt.Errorf("failed to set cur_stake_score and cur_pos_value to 0 %w", err)
+		}
+		ss := s.getStakeScore(p, u.UnlockMCBTime, u.StakedMCB)
+		ui := &mining.UserInfo{
+			Trader:        u.ID,
+			Epoch:         s.thisEpoch,
+			CurPosValue:   pv,
+			CurStakeScore: ss,
+			AccFee:        u.TotalFee,
+		}
+		uis[i] = ui
+	}
+	if err := s.db.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "trader"}, {Name: "epoch"}},
+		DoUpdates: clause.AssignmentColumns([]string{"cur_pos_value", "cur_stake_score", "acc_fee"}),
+	}).Create(&uis).Error; err != nil {
+		return 0, fmt.Errorf("failed to create user_info: size=%v %w", len(uis), err)
+	}
+	// 3. update score
+	var (
+		elapsed   = decimal.NewFromInt((p - s.thisEpochStartTime) / 60)
+		remaining = decimal.NewFromInt((s.thisEpochEndTime - s.thisEpochStartTime) / 60).Sub(elapsed)
+	)
+	var all []*mining.UserInfo
+	if err := s.db.Model(mining.UserInfo{}).Where("epoch=?", s.thisEpoch).Find(&all).Error; err != nil {
+		return 0, fmt.Errorf("fail to fetch all users in this epoch %w", err)
+	}
+	for _, ui := range all {
+		ui.Score = s.getScore(ui, elapsed, remaining)
+	}
+	if err := s.db.Save(&all).Error; err != nil {
+		return 0, fmt.Errorf("failed to create user_info: size=%v %w", len(uis), err)
+	}
+	if err := s.setSyncProgress(np); err != nil {
+		return 0, fmt.Errorf("fail to save sync progress %w", err)
+	}
+	// end tx
+
+	return np, nil
+}
+
+func (s Syncer) getScore(ui *mining.UserInfo, elapsed decimal.Decimal, remain decimal.Decimal) decimal.Decimal {
+	if ui.AccFee.IsZero() {
+		return decimal.Zero
+	}
+	fee := ui.AccFee.Sub(ui.InitFee)
+	if fee.IsZero() {
+		return decimal.Zero
+	}
+	stake := ui.AccStakeScore.Mul(elapsed).Add(ui.CurStakeScore.Mul(remain)).Div(elapsed.Add(remain))
+	if stake.IsZero() {
+		return decimal.Zero
+	}
+	posVal := ui.AccPosValue.Mul(elapsed).Add(ui.CurPosValue.Mul(remain)).Div(elapsed.Add(remain))
+	if posVal.IsZero() {
+		return decimal.Zero
+	}
+	return fee.Pow(s.thisEpochWeightFee).
+		Mul(stake.Pow(s.thisEpochWeightMCB)).
+		Mul(posVal.Pow(s.thisEpochWeightOI))
+}
+
+func (s Syncer) getStakeScore(curTime int64, unlockTime int64, staked decimal.Decimal) decimal.Decimal {
+	if unlockTime < curTime {
+		return decimal.Zero
+	}
+	days := normN(unlockTime-curTime, 86400)
+	return decimal.NewFromInt(days).Mul(staked)
+}
+
+func (s Syncer) getPositionValue(accounts []*MarginAccount, bn int64, cache map[string]*decimal.Decimal) (decimal.Decimal, error) {
+	sum := decimal.Zero
+
+	for _, a := range accounts {
+		var price *decimal.Decimal
+		// 0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13-0-0x00233150044aec4cba478d0bf0ecda0baaf5ad19
+		perpId := strings.Join(strings.Split(a.ID, "-")[:2], "-")
+		if v, ok := cache[perpId]; ok {
+			price = v
+		} else {
+			addr, _, index, err := s.GetPoolAddrIndexUserID(a.ID)
+			if err != nil {
+				return sum, fmt.Errorf("fail to get pool address and index from id %w", err)
+			}
+			p, err := s.GetMarkPriceBasedOnBlockNumber(bn, addr, index)
+			if err != nil {
+				return sum, fmt.Errorf("fail to get mark price %w", err)
+			}
+			price = p
+			cache[perpId] = p
+		}
+		sum = price.Mul(a.Position).Abs()
+	}
+	return sum, nil
+}
+
+func (s *Syncer) GetMarkPriceBasedOnBlockNumber(blockNumber int64, poolAddr string, perpetualIndex int) (*decimal.Decimal, error) {
+	s.logger.Debug("Get mark price based on block number %d, poolAddr %s, perpetualIndex %d", blockNumber, poolAddr, perpetualIndex)
+
+	query := `{
+		markPrices(first: 1, block: { number: %d }, where: {id: "%s"}) {
+    		id
+    		price
+    		timestamp
+		}
+	}`
+	var resp struct {
+		Data struct {
+			MarkPrices []struct {
+				ID    string          `json:"id"`
+				Price decimal.Decimal `json:"price"`
+			}
+		}
+	}
+	id := fmt.Sprintf("%s-%d", poolAddr, perpetualIndex)
+	if err := s.queryGraph(s.mai3GraphUrl, &resp, query, blockNumber, id); err != nil {
+		return nil, fmt.Errorf("fail to get mark price %w", err)
+	}
+	if len(resp.Data.MarkPrices) == 0 {
+		return nil, errors.New("empty mark price")
+	}
+	return &resp.Data.MarkPrices[0].Price, nil
+}
+
+func (s *Syncer) GetMarkPrices(bn int64) (map[string]*decimal.Decimal, error) {
+	s.logger.Debug("Get mark price based on block number %d", bn)
+	// TODO: 1000 may be not enough for all markets, need paging
+	query := `{
+		markPrices(first: 1000, block: { number: %v }) {
+			id
+			price
+			timestamp
+		}
+	}`
+	var resp struct {
+		Data struct {
+			MarkPrices []struct {
+				ID    string           `json:"id"`
+				Price *decimal.Decimal `json:"price"`
+			}
+		}
+	}
+	if err := s.queryGraph(s.mai3GraphUrl, &resp, query, bn); err != nil {
+		return nil, fmt.Errorf("fail to get mark price %w", err)
+	}
+	prices := make(map[string]*decimal.Decimal)
+	for _, p := range resp.Data.MarkPrices {
+		prices[p.ID] = p.Price
+	}
+	return prices, nil
+}
+
+func (s *Syncer) getEpoch() error {
+	// get epoch from schedule database.
+	now := time.Now().Unix()
+	var schedules []*mining.Schedule
+	err := s.db.Model(&mining.Schedule{}).Scan(&schedules).Error
+	if err != nil {
+		s.logger.Error("Failed to get schedule %s", err)
+		return err
+	}
+	if len(schedules) == 0 {
+		return EMPTY_SCHEDULE
+	}
+	for _, schedule := range schedules {
+		if now > schedule.StartTime && now < schedule.EndTime {
+			s.thisEpoch = schedule.Epoch
+			s.thisEpochStartTime = schedule.StartTime
+			s.thisEpochEndTime = schedule.EndTime
+			s.thisEpochWeightFee = schedule.WeightFee
+			s.thisEpochWeightOI = schedule.WeightOI
+			s.thisEpochWeightMCB = schedule.WeightMCB
+			return nil
+		}
+	}
+	return NOT_IN_EPOCH
+}
+
+// TimestampToBlockNumber which is the closest but less than or equal to timestamp
+func (s *Syncer) TimestampToBlockNumber(timestamp int64) (int64, error) {
+	s.logger.Debug("get block number which is the closest but less than or equal to timestamp %d", timestamp)
+	query := `{
+		blocks(
+			first:1, orderBy: number, orderDirection: asc, 
+			where: {timestamp_gt: %d}
+		) {
+			id
+			number
+			timestamp
+		}
+	}`
+	var response struct {
+		Data struct {
+			Blocks []*Block
+		}
+	}
+	// return err when can't get block number in three times
+	if err := s.queryGraph(s.blockGraphUrl, &response, query, timestamp); err != nil {
+		return -1, fmt.Errorf("fail to transform timestamp to block number in three times %w", err)
+	}
+
+	if len(response.Data.Blocks) != 1 {
+		return -1, fmt.Errorf("length of block of response is not equal to 1: expect=1, actual=%v, timestamp=%v", len(response.Data.Blocks), timestamp)
+	}
+	number, err := strconv.Atoi(response.Data.Blocks[0].Number)
+	if err != nil {
+		return -1, fmt.Errorf("failed to convert block number from string to int err:%s", err)
+	}
+	return int64(number - 1), nil
 }
