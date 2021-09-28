5364b6e1e002c84ee3cbddcb8c66815f1ce23445
yangzhao <yangzhao@mcarlo.com>
2021-09-28 18:33:02 +0800

    take ceil of days in calc stake score

--- syncer/syncer_test.go
+++ syncer/syncer_test.go
@@ -23,3 +23,16 @@ func TestSyncer_getScore(t *testing.T) {
 	)
 
 }
+
+func TestSyncer_getStakeScore(t *testing.T) {
+	expect := func(ret bool, errMsg string) {
+		if !ret {
+			t.Error(errMsg)
+		}
+	}
+	s := Syncer{}
+	// 1 x days
+	expect(s.getStakeScore(0, 86400, decimal.NewFromInt(1)).Equals(decimal.NewFromInt(1)), "1")
+	expect(s.getStakeScore(0, 86300, decimal.NewFromInt(1)).Equals(decimal.NewFromInt(1)), "2")
+	expect(s.getStakeScore(0, 86400*2, decimal.NewFromInt(2)).Equals(decimal.NewFromInt(4)), "3")
+}
