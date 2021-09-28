390fd83f754d4cd30350800ba4fe31ad45522d06
ChampFu <champfu@mcdex.io>
2021-09-28 11:44:58 +0800

    update graph function

--- cmd/tmp2/main.go
+++ cmd/tmp2/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/mcdexio/mai3-trade-mining-watcher/api"
+	"github.com/mcdexio/mai3-trade-mining-watcher/common/config"
 	"github.com/mcdexio/mai3-trade-mining-watcher/common/logging"
+	"github.com/mcdexio/mai3-trade-mining-watcher/syncer"
 	"golang.org/x/sync/errgroup"
 	"os"
 	"os/signal"
@@ -21,18 +24,54 @@ func main() {
 	backgroundCtx, stop := context.WithCancel(context.Background())
 	group, ctx := errgroup.WithContext(backgroundCtx)
 
-	tmServer := api.NewTMServer(ctx, logger)
-	group.Go(func() error {
-		return tmServer.Run()
-	})
+	syn := syncer.NewSyncer(
+		ctx,
+		logger,
+		config.GetString("MAI3_TRADE_MINING_GRAPH_URL"),
+		config.GetString("ARB_BLOCKS_GRAPH_URL"),
+	)
 
-	go WaitExitSignalWithServer(stop, logger, tmServer)
+	priceMap, err := syn.GetMarkPrices(4933593)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(priceMap)
+
+	blockNumber, err := syn.TimestampToBlockNumber(1632674652)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	user, err := syn.GetUsersBasedOnBlockNumber(blockNumber)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(len(user))
+
+	price, err := syn.GetMarkPriceBasedOnBlockNumber(2771249, "0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13", 0)
+	if err != nil {
+		return
+	}
+	fmt.Println(price.String())
+
+	// poolAddr, userId, perpetualIndex, err := syn.GetPoolAddrIndexUserID("0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13-0-0x00233150044aec4cba478d0bf0ecda0baaf5ad19")
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(poolAddr)
+	// fmt.Println(userId)
+	// fmt.Println(perpetualIndex)
+
+	go WaitExitSignal(stop, logger)
+	syn.Init()
+	group.Go(func() error {
+		return syn.Run()
+	})
 
 	if err := group.Wait(); err != nil {
 		logger.Critical("service stopped: %s", err)
 	}
-
-	return
 }
 
 func WaitExitSignal(ctxStop context.CancelFunc, logger logging.Logger) {
