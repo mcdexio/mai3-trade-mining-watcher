d5c9cf4798a2b367e2c2361ff7e52de26077f809
yangzhao <yangzhao@mcarlo.com>
2021-09-28 18:04:08 +0800

    transaction && score calc

--- database/db/transactor.go
+++ database/db/transactor.go
@@ -8,23 +8,6 @@ import (
 
 type TransactionFunc = func(db *gorm.DB) error
 
-type TransactorOption interface {
-	Set(opt *sql.TxOptions)
-}
-
-type OptionReadOnly struct {
-}
-
-func (o OptionReadOnly) Set(opt *sql.TxOptions) {
-	opt.ReadOnly = true
-}
-
-type OptionRepeatableRead struct{}
-
-func (o OptionRepeatableRead) Set(opt *sql.TxOptions) {
-	opt.Isolation = sql.LevelRepeatableRead
-}
-
 func WithTransaction(db *gorm.DB, p TransactionFunc, opts ...*sql.TxOptions) error {
 	tx := db.Begin(opts...)
 	defer func() {
--- syncer/utils.go
+++ syncer/utils.go
@@ -1,1 +1,9 @@
 package syncer
+
+func norm(ts int64) int64 {
+	return ts - ts%60
+}
+
+func normN(ts int64, inv int64) int64 {
+	return ts - ts%inv
+}
