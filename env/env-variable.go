75d0ba48128e6111eaeaf716a64f2b551bd9e4a8
ChampFu <champfu@mcdex.io>
2021-09-30 20:07:24 +0800

    update inverse contract

--- env/env-variable.go
+++ env/env-variable.go
@@ -1,6 +1,9 @@
 package env
 
-import "github.com/mcdexio/mai3-trade-mining-watcher/common/config"
+import (
+	"fmt"
+	"github.com/mcdexio/mai3-trade-mining-watcher/common/config"
+)
 
 // IsCI returns true if we are in CI mode.
 func IsCI() bool {
@@ -18,4 +21,20 @@ func LogColor() bool {
 func ResetDatabase() bool {
 	reset := config.GetString("RESET_DATABASE")
 	return reset == "true"
+}
+
+var whiteList map[string]bool
+
+// InInverseContractWhiteList return true if this contract is inverse
+func InInverseContractWhiteList(marginAccountID string) bool {
+	if len(whiteList) != 0 {
+		return whiteList[marginAccountID]
+	}
+	whiteList = make(map[string]bool)
+	total := config.GetInt("COUNT_INVERSE_CONTRACT_WHITELIST", 0)
+	for i := 0; i < total; i++ {
+		addr := config.GetString(fmt.Sprintf("INVERSE_CONTRACT_WHITELIST%d", i))
+		whiteList[addr] = true
+	}
+	return whiteList[marginAccountID]
 }
