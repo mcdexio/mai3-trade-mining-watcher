78b0700ea082651946180b3f5a5cf53e07cb9d84
ChampFu <champfu@mcdex.io>
2021-10-25 11:54:32 +0800

    move util function

--- syncer/utils.go
+++ syncer/utils.go
@@ -2,6 +2,9 @@ package syncer
 
 import (
 	"fmt"
+	"github.com/mcdexio/mai3-trade-mining-watcher/database/models/mining"
+	"github.com/shopspring/decimal"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -24,4 +27,90 @@ func splitMarginAccountID(marginAccountID string) (poolAddr, userId string, perp
 	poolAddr = rest[0]
 	userId = rest[2]
 	return
+}
+
+func getEstimatedStakeScore(
+	nowTimestamp int64, epoch *mining.Schedule, unlockTime int64,
+	currentStakingReward decimal.Decimal,
+) decimal.Decimal {
+	// s.logger.Debug("nowTS %d, epochEndTime %d, unlockTime %d", nowTimestamp, epoch.EndTime, unlockTime)
+	// A = (1 - Floor(RemainEpochSeconds / 86400) / UnlockTimeInDays / 2) * CurrentStakingReward * RemainEpochMinutes
+	// EstimatedAverageStakingScore  = (CumulativeStakingScore + A) / TotalEpochMinutes
+
+	// floor to 0 if less than 1 day
+	endTimeMinusNowTS := float64(epoch.EndTime-nowTimestamp)
+	if endTimeMinusNowTS <= 0 {
+		// there is no remainTime in this epoch
+		return decimal.Zero
+	}
+	remainEpochDays := math.Floor(endTimeMinusNowTS / 86400)
+	// s.logger.Debug("remainEpochDays %v", remainEpochDays)
+	// ceil to 1 if less than 1 day
+	unlockTimeInDays := math.Ceil(float64(unlockTime-nowTimestamp) / 86400)
+	// s.logger.Debug("unlockTimeInDays %v", unlockTimeInDays)
+	var remainProportion decimal.Decimal
+	if unlockTimeInDays <= 0 {
+		// there is no stake time
+		return decimal.Zero
+	} else {
+		remainProportion = decimal.NewFromFloat(1.0 - (remainEpochDays / unlockTimeInDays / 2.0))
+	}
+	// s.logger.Debug("remainProportion %v", remainProportion)
+	// ceil to 1 if less than 1 minute
+	remainEpochMinutes := decimal.NewFromFloat(math.Ceil(endTimeMinusNowTS / 60))
+	// s.logger.Debug("remainEpochMinutes %v", remainEpochMinutes)
+	estimatedSS := remainProportion.Mul(currentStakingReward).Mul(remainEpochMinutes)
+	// s.logger.Debug("estimatedStakeScore %v", estimatedSS)
+	return estimatedSS
+}
+
+func getStakeScore(curTime int64, unlockTime int64, staked decimal.Decimal) decimal.Decimal {
+	// ss is (unlock time - now) * u.StackedMCB <=> s = n * t
+	if unlockTime < curTime {
+		return decimal.Zero
+	}
+	// floor to 1 if less than 1 day
+	days := int64(math.Ceil(float64(unlockTime-curTime) / 86400))
+	return decimal.NewFromInt(days).Mul(staked)
+}
+
+func getScore(epoch *mining.Schedule, ui *mining.UserInfo, remains decimal.Decimal) decimal.Decimal {
+	if ui.AccTotalFee.IsZero() {
+		return decimal.Zero
+	}
+	fee := decimal.Zero
+	if epoch.Epoch == 0 {
+		// epoch 0 is totalFee
+		fee = ui.AccTotalFee.Sub(ui.InitTotalFee)
+	} else {
+		fee = ui.AccFee.Sub(ui.InitFee)
+	}
+	if fee.LessThanOrEqual(decimal.Zero) {
+		return decimal.Zero
+	}
+	stake := ui.AccStakeScore.Add(ui.CurStakeScore)
+	stake = stake.Add(ui.EstimatedStakeScore)
+	if stake.LessThanOrEqual(decimal.Zero) {
+		return decimal.Zero
+	}
+	// EstimatedOpenInterest = (CumulativeOpenInterest + CurrentOpenInterest * RemainEpochMinutes) / TotalEpochMinutes
+	posVal := ui.AccPosValue.Add(ui.CurPosValue.Mul(remains))
+	if posVal.LessThanOrEqual(decimal.Zero) {
+		return decimal.Zero
+	}
+	// ceil to 1 if less than 1 minute
+	totalEpochMinutes := math.Ceil(float64(epoch.EndTime-epoch.StartTime) / 60)
+
+	// decimal package has issue on pow function
+	wFee, _ := epoch.WeightFee.Float64()
+	wStake, _ := epoch.WeightMCB.Float64()
+	wPos, _ := epoch.WeightOI.Float64()
+	feeFloat, _ := fee.Float64()
+	stakeFloat, _ := stake.Float64()
+	posValFloat, _ := posVal.Float64()
+	score := math.Pow(feeFloat, wFee) * math.Pow(stakeFloat/totalEpochMinutes, wStake) * math.Pow(posValFloat/totalEpochMinutes, wPos)
+	if math.IsNaN(score) {
+		return decimal.Zero
+	}
+	return decimal.NewFromFloat(score)
 }
